61317e7d6768f0b52e535a387c3b2d22199cdddc
Moorgtat <mat.lecardinal@gmail.com>
2019-10-25 14:06:23 +0200

    [front-portfolio] Ajout des modules 'vue-router' 'vuex' 'veevalidate' 'axios' 'buefy' à notre projet vue.js.

--- front-portfolio/src/main.js
+++ front-portfolio/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import VeeValidate from 'vee-validate';
+import Buefy from 'buefy'
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
+Vue.use(VeeValidate);
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
