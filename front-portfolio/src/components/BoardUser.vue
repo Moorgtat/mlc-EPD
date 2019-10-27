27719882271b707276aab060f51e296afcc654f0
Moorgtat <mat.lecardinal@gmail.com>
2019-10-27 12:09:46 +0100

    [front-portfolio] Modif User Content.

--- front-portfolio/src/components/BoardUser.vue
+++ front-portfolio/src/components/BoardUser.vue
@@ -2,6 +2,8 @@
     <div class="container">
         <header class="jumbotron">
             <h3>{{content}}</h3>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A blanditiis eligendi modi quia rerum. Fugit natus provident, repellat soluta suscipit voluptates voluptatum? Error iure laboriosam modi nemo, omnis recusandae voluptatibus!</p>
         </header>
     </div>
 </template>
