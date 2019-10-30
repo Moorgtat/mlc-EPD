5442d916d3623c1dd3d7dea4b73b3f800a64089c
Moorgtat <mat.lecardinal@gmail.com>
2019-10-30 16:00:57 +0100

    [back-portfolio] Implémentation Methode pour acces au public Content dans AccesCOntroller et méthode find ajoutée dans IdentityRepository.

--- back-portfolio/src/main/java/com/mlc/portfolio/controllers/AccesController.java
+++ back-portfolio/src/main/java/com/mlc/portfolio/controllers/AccesController.java
@@ -1,18 +1,48 @@
 package com.mlc.portfolio.controllers;
 
+import com.mlc.portfolio.models.Identity;
+import com.mlc.portfolio.models.Projekt;
+import com.mlc.portfolio.models.Skill;
+import com.mlc.portfolio.repository.IdentityRepository;
+import com.mlc.portfolio.repository.ProjektRepository;
+import com.mlc.portfolio.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/acces")
 public class AccesController {
+
+	@Autowired
+	private IdentityRepository identityRepository;
+
+	@Autowired
+	private ProjektRepository projektRepository;
+
+	@Autowired
+	private SkillRepository skillRepository;
+
 	@GetMapping("/all")
-	public String allAccess() {
-		return "Public Content.";
+	public ResponseEntity<?> allAccess() {
+		Identity identity = identityRepository.findFirstById(1);
+		List <Projekt> projekts = projektRepository.findAll();
+		List <Skill> skills = skillRepository.findAll();
+		Map<String, Object> result = new HashMap<String,Object>();
+		result.put("identity", identity);
+		result.put ("projekts", projekts);
+		result.put ("skills", skills);
+		return new ResponseEntity<Map<String,Object>>(result, HttpStatus.OK);
 	}
 	
 	@GetMapping("/user")
--- back-portfolio/src/main/java/com/mlc/portfolio/repository/IdentityRepository.java
+++ back-portfolio/src/main/java/com/mlc/portfolio/repository/IdentityRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IdentityRepository extends JpaRepository<Identity, Long> {
 
+    Identity findFirstById(Integer id);
 }
