c391d9f76eb33dc708e4490339574fb3b359e9b8
Moorgtat <mat.lecardinal@gmail.com>
2019-12-06 18:22:09 +0100

    [back-portfolio] Suppression parties inutiles dans SpringBootApp.

--- back-portfolio/src/main/java/com/mlc/portfolio/SpringBootPortfolioApplication.java
+++ back-portfolio/src/main/java/com/mlc/portfolio/SpringBootPortfolioApplication.java
@@ -1,11 +1,7 @@
 package com.mlc.portfolio;
 
 //import com.mlc.portfolio.models.Identity;
-//import com.mlc.portfolio.models.Projekt;
-//import com.mlc.portfolio.models.Skill;
 //import com.mlc.portfolio.repository.IdentityRepository;
-//import com.mlc.portfolio.repository.ProjektRepository;
-//import com.mlc.portfolio.repository.SkillRepository;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,12 +12,6 @@ public class SpringBootPortfolioApplication implements CommandLineRunner{
 
 //	@Autowired
 //	private IdentityRepository identityRepository;
-//
-//	@Autowired
-//	private ProjektRepository projektRepository;
-//
-//	@Autowired
-//	private SkillRepository skillRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPortfolioApplication.class, args);
@@ -36,30 +26,6 @@ public class SpringBootPortfolioApplication implements CommandLineRunner{
 //				"mat.lecardinal@gmail.com",
 //				"github",
 //				"twitter"));
-//
-//		Skill skill1 = skillRepository.save(new Skill("Titre Skill1",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-//				));
-//
-//		Skill skill2 = skillRepository.save(new Skill("Titre Skill2",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-//				));
-//
-//		Projekt projekt1 = projektRepository.save(new Projekt("Projet 1",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"GitHub"));
-//
-//		Projekt projekt2 = projektRepository.save(new Projekt("Projet 2",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"GitHub"));
-//
-//		Projekt projekt3 = projektRepository.save(new Projekt("Projet 3",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"GitHub"));
-//
-//		Projekt projekt4 = projektRepository.save(new Projekt("Projet 4",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"GitHub"));
 	}
 
 }
